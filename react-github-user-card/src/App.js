08139a4f7401fd15835901998e22459315bbd418
unknown <prakashbahadursingh2@gmail.com>
2020-05-06 02:30:01 -0700

    component function created with stating initial state, axios get request successful to fetch user data inside the componentDidMount method, handleChange method created to allow text input and, findUser function created using axios get request whihc allow disply of user up on typing username in input bos followed by click on find user button

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import UserCard from './UserCard'
+
+
+export default class App extends React.Component {
+  state = {
+    userText: '',
+    users: []
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/prakashbasingh")
+      .then(res => {
+        console.log(res.data)
+        this.setState({ users: res.data});
+      })
+      .catch(err => console.log(err));
+  }
+
+  handleChanges = e => {
+    this.setState({
+      userText: e.target.value
+    });
+  };
+  
+  findUsers = e => {
+    e.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${this.state.userText}`)
+      .then(res => {
+        this.setState({ users: res.data});
+      })
+      .catch(err => console.log(err));
+  };
+  
+  render() {
+    // console.log('rendering');
+    return (
+      <div className="App" style = {{textAlign: 'center'}}>
+        <h1>Github User</h1>
+        <input
+          type="text"
+          value={this.state.userText}
+          onChange={this.handleChanges}
+        />
+        <button onClick={this.findUsers}>Find Users</button>
+        <div className="users" >
+          <img src={this.state.users.avatar_url} alt='user picture'/>
+          <h3>Name: {this.state.users.name}</h3>
+          <p>Username: {this.state.users.login}</p>
+          <p>Follower: {this.state.users.followers}</p>         
+        
+          {/* {this.state.users.map(user => (
+            <UserCard users={this.state.users}/>
+          ))} */}
+        </div>
+      </div>
+    );
+  }
+
 }
-
-export default App;
